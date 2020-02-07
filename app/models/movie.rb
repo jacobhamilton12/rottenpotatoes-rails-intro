81483110fe5f0730ecf0c3fd5a376ab04aead8f8
Jacob Hamilton <hamilton1jake@gmail.com>
2020-02-07 17:50:06 +0000

    part2 test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
     def self.ratings
-        Movie.select(:rating).distinct.sort.inject([]) { |a, b| a.push(b.rating)}
+        Movie.select(:rating).distinct.sort.reverse.inject([]) { |a, b| a.push(b.rating)}
     end
 end
