3a90631fe32bfbeca6ba5ee4ebf1fae8522bb405
Jacob Hamilton <hamilton1jake@gmail.com>
2020-02-07 20:00:30 +0000

    part2 and 3 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,7 +15,7 @@ class MoviesController < ApplicationController
     params[:sort].nil? ? (session[:sort].nil? ? @sort = :title : @sort = session[:sort]) : @sort = params[:sort]
     params[:ratings].nil? ? (session[:ratings].nil? ? @r = @all_ratings : @r = session[:ratings]) : @r = params[:ratings].keys
     @movies = Movie.where(rating: @r).order(@sort)
-    
+  
     session[:ratings] = @r
     session[:sort] = @sort
   end
