ed6231a7cf69e4c12a434fdc1ba2ceeeaa4f9ed3
Jacob Hamilton <hamilton1jake@gmail.com>
2020-02-07 19:49:22 +0000

    part2 test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,8 +12,8 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.ratings
-    params[:sort].nil? ? session[:sort].nil? ? @sort = :title : @sort = session[:sort] : @sort = params[:sort]
-    params[:ratings].nil? ? session[:ratings].nil? ? @r = @all_ratings : @r = session[:ratings] : @r = all_ratings
+    params[:sort].nil? ? (session[:sort].nil? ? @sort = :title : @sort = session[:sort]) : @sort = params[:sort]
+    params[:ratings].nil? ? (session[:ratings].nil? ? @r = @all_ratings : @r = session[:ratings]) : @r = @all_ratings
     @movies = Movie.where(rating: @r).order(@sort)
     
     session[:ratings] = @r
