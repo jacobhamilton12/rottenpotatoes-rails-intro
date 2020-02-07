a350761c519b0319f68cb247824668e318263f22
Jacob Hamilton <hamilton1jake@gmail.com>
2020-02-07 18:56:00 +0000

    part2 test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @r = []
     @all_ratings = Movie.ratings
     @sort = params[:sort]
     params[:ratings].nil? ? @r = @all_ratings : @r = params[:ratings].keys
